691074e4a35a3ad0611d3342f096d804a1dd3275
Francisco Garcia <jjjchico1@gmail.com>
2023-03-09 17:54:22 -0600

    fix: comment connection DDBB

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,15 @@
 import mysql, { Pool, PoolConnection } from 'mysql2/promise'
 import { propertiesConectionDB } from './config'
 
+/* using the singleton design pattern to have a single instance of the database connection */
 class Database {
+  // singleton instance to the database connection
   private static instance: Database
   private pool: Pool | null = null
 
   private constructor() {}
 
+  // obtain the instance of the database connection
   public static getInstance(): Database {
     if (!Database.instance) {
       Database.instance = new Database()
@@ -15,6 +18,7 @@ class Database {
     return Database.instance
   }
 
+  // connect to the database
   public async connect(): Promise<PoolConnection> {
     if (!this.pool) {
       this.pool = mysql.createPool(propertiesConectionDB)
@@ -23,6 +27,7 @@ class Database {
     return this.pool.getConnection()
   }
 
+  // disconnect from the database
   public async disconnect(): Promise<void> {
     if (this.pool) {
       try {
