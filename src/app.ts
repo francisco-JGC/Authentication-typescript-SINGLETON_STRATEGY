7232531d19e97549574ab1daf31b3a3f40f2bdb1
Francisco Garcia <jjjchico1@gmail.com>
2023-03-10 09:15:12 -0600

    fix: anyway

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,10 @@
 import 'dotenv/config'
 import express from 'express'
-import db from './database'
 
 const app = express()
 app.set('port', process.env.PORT_APP || 5000)
 
-const loadRoutes = () => {
-  // Load all routes
-}
-
-const loadMiddlewares = () => {
-  // Load all middlewares
-}
-
-const loadDatabase = async () => {
-  // Load database connection
-  const connection = await db.connect()
-
-  if (connection) {
-    console.log('Connected to MySQL database')
-  } else {
-    console.log('Error connecting to MySQL database')
-  }
-}
-
-const loadServices = async () => {
-  // Load all services
-  loadRoutes()
-  loadMiddlewares()
-  await loadDatabase()
-}
-
-loadServices()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 export default app
