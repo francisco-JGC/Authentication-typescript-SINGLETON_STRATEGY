faa217567d4167d5a770af1cb81fd08b6b59ac6c
Francisco Garcia <jjjchico1@gmail.com>
2023-03-23 22:44:35 -0600

    fix: use handles Response

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,32 +1,33 @@
 import { Request, Response } from 'express'
 import { UsersModel } from '../models/users'
+import { handleInternalServerError, handleOK } from '../utils/handleErrorModels'
 export class UsersController {
   private usersModel: UsersModel = new UsersModel()
 
   public async getUsers(req: Request, res: Response): Promise<void> {
     try {
       const users = await this.usersModel.getUsers()
-      res.status(200).json(users)
-    } catch (err) {
-      res.status(500).json(err)
+      res.send(await handleOK(users))
+    } catch (err: any) {
+      res.send(await handleInternalServerError(err.message))
     }
   }
   public async insertUser(req: Request, res: Response): Promise<void> {
     try {
       const user = req.body
       const result = await this.usersModel.insertUser(user)
-      res.status(200).json(result)
-    } catch (err) {
-      res.status(500).json(err)
+      res.send(await handleOK(result))
+    } catch (err: any) {
+      res.send(await handleInternalServerError(err.message))
     }
   }
   public async getUserById(req: Request, res: Response): Promise<void> {
     try {
       const id = req.params.id
       const user = await this.usersModel.getUserById(id)
-      res.status(200).json(user)
-    } catch (err) {
-      res.status(500).json(err)
+      res.send(await handleOK(user))
+    } catch (err: any) {
+      res.send(await handleInternalServerError(err.message))
     }
   }
   public async updateUserById(req: Request, res: Response): Promise<void> {
@@ -34,9 +35,9 @@ export class UsersController {
       const id = req.params.id
       const user = req.body
       const result = await this.usersModel.updateUserById(id, user)
-      res.status(200).json(result)
-    } catch (err) {
-      res.status(500).json(err)
+      res.send(await handleOK(result))
+    } catch (err: any) {
+      res.send(await handleInternalServerError(err.message))
     }
   }
 }
