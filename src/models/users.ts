6f349859b783d3bc8a93f84d8a2a86c898ba11d5
Francisco Garcia <jjjchico1@gmail.com>
2023-03-23 21:52:34 -0600

    fix: use the error handle

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,25 +1,61 @@
 import { Context } from '../db/context'
 import { InsertStrategy } from '../db/strategies/insertStrategy'
 import { SelectStrategy } from '../db/strategies/selectStrategy'
+import {
+  handleInternalServerError,
+  handleNotFound
+} from '../utils/handleErrorModels'
 
 export class UsersModel {
-  public async getUsers(): Promise<any> {
-    const context = new Context(new SelectStrategy())
-    return await context.execute('SELECT * FROM users', [])
+  async getUsers(): Promise<any> {
+    try {
+      const context = new Context(new SelectStrategy())
+      return await context.execute('SELECT * FROM users', [])
+    } catch (error: any) {
+      return await handleInternalServerError(error.message)
+    }
   }
 
-  public async insertUser(user: any): Promise<any> {
-    const context = new Context(new InsertStrategy())
-    return await context.execute('INSERT INTO users SET ?', [user])
+  async insertUser(user: any): Promise<any> {
+    try {
+      const context = new Context(new InsertStrategy())
+      return await context.execute('INSERT INTO users SET ?', [user])
+    } catch (error: any) {
+      return await handleInternalServerError(error.message)
+    }
   }
 
-  public async getUserById(id: string): Promise<any> {
-    const context = new Context(new SelectStrategy())
-    return await context.execute('SELECT * FROM users WHERE id = ?', [id])
+  async getUserById(id: string): Promise<any> {
+    try {
+      const context = new Context(new SelectStrategy())
+      const response = await context.execute(
+        'SELECT * FROM users WHERE id = ?',
+        [id]
+      )
+      if (response.length === 0) {
+        return await handleNotFound()
+      }
+
+      return response[0]
+    } catch (error: any) {
+      return await handleInternalServerError(error.message)
+    }
   }
 
-  public async updateUserById(id: string, user: any): Promise<any> {
-    const context = new Context(new InsertStrategy())
-    return await context.execute('UPDATE users SET ? WHERE id = ?', [user, id])
+  async updateUserById(id: string, user: any): Promise<any> {
+    try {
+      const context = new Context(new InsertStrategy())
+      const response = await context.execute(
+        'UPDATE users SET ? WHERE id = ?',
+        [user, id]
+      )
+      if (response.affectedRows === 0) {
+        return await handleNotFound()
+      }
+
+      return await this.getUserById(id)
+    } catch (error: any) {
+      return await handleInternalServerError(error.message)
+    }
   }
 }
